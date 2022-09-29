8005d1deb04a4443a92f337f4020d99ea87b2d18
Torens29 <44534255+Torens29@users.noreply.github.com>
2022-09-29 18:33:25 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("applicationForm").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("applicationForm").style.display = "none";
+}
